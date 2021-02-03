b9917c42cadbb5136af3f8a5ec27b4740d2a0c55
Darpan Kulkarni <darpankulkarni@yahoo.in>
2021-02-03 15:45:00 +0530

    Initial commit

---
+++ includes/class-dck-cf7-simple-redirection-activator.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://john.doe
+ * @since      1.0.0
+ *
+ * @package    Dck_Cf7_Simple_Redirection
+ * @subpackage Dck_Cf7_Simple_Redirection/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Dck_Cf7_Simple_Redirection
+ * @subpackage Dck_Cf7_Simple_Redirection/includes
+ * @author     John Doe <johndoe@example.com>
+ */
+class Dck_Cf7_Simple_Redirection_Activator
+{
+
+    /**
+     * Short Description. (use period)
+     *
+     * Long Description.
+     *
+     * @since    1.0.0
+     */
+    public static function activate()
+    {
+
+    }
+
+}
---
+++ includes/class-dck-cf7-simple-redirection-deactivator.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://john.doe
+ * @since      1.0.0
+ *
+ * @package    Dck_Cf7_Simple_Redirection
+ * @subpackage Dck_Cf7_Simple_Redirection/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Dck_Cf7_Simple_Redirection
+ * @subpackage Dck_Cf7_Simple_Redirection/includes
+ * @author     John Doe <johndoe@example.com>
+ */
+class Dck_Cf7_Simple_Redirection_Deactivator
+{
+
+    /**
+     * Short Description. (use period)
+     *
+     * Long Description.
+     *
+     * @since    1.0.0
+     */
+    public static function deactivate()
+    {
+
+    }
+
+}
